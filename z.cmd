820ccd5cb4f86d53541694c7b924f7115ad4cb68
John Duimovich <jduimovich@gmail.com>
2021-02-10 15:40:11 -0500

    test

---
+++ z.cmd
@@ -0,0 +1,6 @@
+
+export GITHUB_OWNER=jduimovich
+export GITHUB_REPOSITORY=debugdays
+export RUNNER_LABELS=crc
+export GITHUB_PAT=2d8114aa5fa8a5b7c8e78fbddb03e84be5150536
+docker run -it --entrypoint /bin/bash quay.io/redhat-github-actions/runner
